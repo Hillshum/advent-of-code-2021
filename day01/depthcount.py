e1eb183f2ad1a2bd13c5e0177ed53ce5a77d6e99
Hilton Shumway <hillshum@gmail.com>
2021-12-14 18:58:15 -0600

    Day 01.2

--- day01/depthcount.py
+++ day01/depthcount.py
@@ -28,3 +28,36 @@ def part1():
             count += 1
 
     print(count)
+
+def part2():
+    sums = []
+    for i, line in enumerate(lines):
+        if i is 0:
+            continue
+        if i is 1:
+            continue
+        prev = int(lines[i-1].strip())
+        prev2 = int(lines[i-2].strip())
+        curr = int(line.strip())
+        print(curr, prev, prev2)
+        sums.append(curr + prev + prev2) 
+
+
+    print(sums)
+
+    count = 0
+    for i, line in enumerate(sums):
+        if i is 0:
+            continue
+        prev = int(sums[i-1])
+        curr = int(line)
+        print(curr, prev)
+        if curr > prev:
+            count += 1
+
+    print(count)
+
+
+
+if __name__ == '__main__':
+    part2()
