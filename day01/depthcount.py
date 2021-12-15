25ccbdf7fb45b1bc4621e7be6a66fae7320e3468
Hilton Shumway <hillshum@gmail.com>
2021-12-14 18:50:56 -0600

    complete day 01

---
+++ day01/depthcount.py
@@ -0,0 +1,29 @@
+
+
+with open('input.txt', 'r') as f:
+    lines = f.readlines()
+
+# lines = lines[:50]
+
+# lines = ['199',
+# '200',
+# '208',
+# '210',
+# '200',
+# '207',
+# '240',
+# '269',
+# '260',
+# '263']
+
+count = 0
+for i, line in enumerate(lines):
+    if i is 0:
+        continue
+    prev = int(lines[i-1].strip())
+    curr = int(line.strip())
+    print(curr, prev)
+    if curr > prev:
+        count += 1
+
+print(count)
