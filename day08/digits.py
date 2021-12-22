d6e3a6e38a83522c419db5927ff0c03c773f6ee9
Hilton Shumway <hillshum@gmail.com>
2021-12-22 09:55:56 -0600

    day08.2

--- day08/digits.py
+++ day08/digits.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from collections import defaultdict
 from typing import List
 
 input_string = """be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
@@ -19,6 +20,16 @@ with open('input.txt', 'r') as f:
 UNIQUES = set([2, 3, 4, 7])
 display_strings = input_string.strip().split('\n')
 
+LENGHTS = {
+    2: [1],
+    3: [7],
+    4: [4],
+    5: [2,3,5],
+    6: [0,6,9],
+    7: [8]
+    
+}
+
 @dataclass
 class Display:
     inputs: List[str]
@@ -28,6 +39,63 @@ class Display:
     def from_string(cls, s):
         ins, outs = s.split('|')
         return Display(ins.strip().split(' '), outs.strip().split(' '))
+
+    def parse(self):
+        by_length = defaultdict(list) 
+        for i in self.inputs:
+            by_length[len(i)].append(i) 
+        print(by_length)
+
+        mappings = {}
+
+        mappings[7] = set(by_length[3][0])
+        mappings[1] = set(by_length[2][0])
+        # mappings['a'] = seven - one
+
+        mappings[6] = set(next(filter(lambda x:  not (set(x) > mappings[1]), by_length[6])))
+        by_length[6] = [x for x in by_length[6] if set(x) != mappings[6] ]
+        # print(by_length, six)
+        # mappings['c'] = one - six
+
+        mappings[4] = set(by_length[4][0])
+        # print(four)
+
+        mappings[9] = set(next(filter(lambda x: set(x) > mappings[4], by_length[6])))
+        by_length[6] = [x for x in by_length[6] if set(x) != mappings[9] ]
+
+        mappings[8] = set(by_length[7][0])
+
+        mappings[0] = set(by_length[6][0])
+
+        # mappings['e'] = eight - nine
+        # print(nine)
+
+        # mappings['d'] = eight - zero
+
+        mappings[3] = set(next(filter(lambda x: set(x) > mappings[1], by_length[5])))
+        by_length[5] = [x for x in by_length[5] if set(x) != mappings[3] ]
+
+        # mappings['g'] = three - four - seven
+
+
+        mappings[5] = set(next(filter(lambda x: set(x) < mappings[6], by_length[5])))
+        by_length[5] = [x for x in by_length[5] if set(x) != mappings[5] ]
+
+        # mappings['e'] = six - five
+
+
+        mappings[2] = set(by_length[5][0])
+
+
+
+
+
+        # print(mappings)
+        return {frozenset(v): k for k,v in mappings.items()}
+
+    def decode_output(self):
+        mappings = self.parse()
+        return int("".join([str(mappings[frozenset(i)]) for i in self.outputs]))
 
 
 displays = [*map(Display.from_string, display_strings)]
@@ -44,3 +112,9 @@ print(target)
 # output_lens = map(len)
 
 # print(displays)
+test_display = Display.from_string('acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf')
+print(test_display.decode_output())
+
+decoded_output = sum([d.decode_output() for d in displays])
+
+print(decoded_output)
